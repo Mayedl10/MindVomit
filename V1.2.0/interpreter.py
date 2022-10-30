473cbf8edf96bc42b77d234ddc1c6de3044bf2aa
Mayedl10 <109928619+Mayedl10@users.noreply.github.com>
2022-10-30 20:29:03 +0100

    Update interpreter.py

--- V1.2.0/interpreter.py
+++ V1.2.0/interpreter.py
@@ -88,7 +88,10 @@ def interpret(code):
             ptr = memory[ptr]
 
         elif code[iterPos] == ";": #set current memory to pointer position
-            memory[ptr] = ptr
+            if 0 <= ptr <= 255:
+                memory[ptr] = ptr
+            else:
+                memory[ptr] = 255
 
         elif code[iterPos] == "z": #set current memory to 0
             memory[ptr] = 0
